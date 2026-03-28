85e188220055bed98b7bc132f4078fcc73101f2f
morgan11d <morkicel@gmail.com>
2026-03-28 10:50:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,17 +1,59 @@
-// Copyright 2025 NNTU-CS
+#include <iostream>
 #include <cstdint>
-#include "alg.h"
-
-uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
-}
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+    unsigned int length = 1;
+    
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num = num / 2;
+        } else {
+            num = 3 * num + 1;
+        }
+        length++;
+    }
+    
+    return length;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen,
-                        uint64_t lbound,
-                        uint64_t rbound) {
-  return 1;
+uint64_t collatzMaxValue(uint64_t num) {
+    uint64_t maxVal = num;
+    
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num = num / 2;
+        } else {
+            num = 3 * num + 1;
+        }
+        
+        if (num > maxVal) {
+            maxVal = num;
+        }
+    }
+    
+    return maxVal;
+}
+
+unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound, uint64_t rbound) {
+    unsigned int bestNum = lbound;
+    unsigned int bestLen = collatzLen(lbound);
+    uint64_t bestMaxVal = collatzMaxValue(lbound);
+    
+    for (uint64_t num = lbound + 1; num <= rbound; num++) {
+        unsigned int currentLen = collatzLen(num);
+        
+        if (currentLen > bestLen) {
+            bestLen = currentLen;
+            bestNum = num;
+            bestMaxVal = collatzMaxValue(num);
+        }
+    }
+    
+    *maxlen = bestLen;
+    
+    std::cout << bestNum << std::endl;
+    std::cout << bestMaxVal << std::endl;
+    std::cout << bestLen << std::endl;
+    
+    return bestNum;
 }
